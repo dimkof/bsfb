febc4060dd867526ca09d1bd3afcab623a0f02e8
Owen Lees <sales@sitesurfer.co.uk>
2014-01-31 22:13:27 +0000

    Fixed the demo - need to add bs3

--- demo.php
+++ demo.php
@@ -1,21 +1,16 @@
 <?php
 require_once ("Bsfb.php");
 
-$form = new Bsfb("agent_edit");
-$form->setTarget("/site_mods/agenthunter/system/agent_details.php","POST");
-$form->setGrid(array(2,1,1));
+$form = new Bsfb("login");
+$form->setTarget("login.php","POST");
+$form->setGrid(array(2));
 
 
-$form->addControl(array("id" => "ag_agentname","value" => "","placeholder" => "Agent Name","type" => "text"));
-
-$form->addControl(array("id" => "ag_photo","value" => "","placeholder" => "Agent Photo","type" => "text"));
-$form->addControl(array("id" => "ag_pers_email","value" => "","placeholder" => "Personal Email","type" => "text"));
-
-$form->addControl(array("id" => "ag_shortbio","value" => "","placeholder" => "Short Biography","type" => "textarea", "rows" => 5));
-
+$form->addControl(array("id" => "email","value" => "","placeholder" => "Login Email","type" => "text"));
+$form->addControl(array("id" => "password","value" => "","placeholder" => "Password","type" => "password"));
 
 //add buttons
-$form->addButton(array("id" => "form_submit", "value" => "Save Changes","version" => "success", "type" => "submit", "css" => "pull-right "));
+$form->addButton(array("id" => "form_submit", "value" => "Login","version" => "success", "type" => "submit", "css" => "pull-right "));
 $form->addButton(array("id" => "form_cancel", "value" => "Cancel","version" => "warning", "type" => "cancel", "css" => "pull-right "));
 
 //show the form onscreen
